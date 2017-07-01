96540c05a6ba3239a47ec613939cf58c6501be79
birdben <191654006@163.com>
2017-07-01 18:58:47 +0800

    Maven构建可执行的jar包并且读取配置文件

--- src/main/java/com/birdben/App.java
+++ src/main/java/com/birdben/App.java
@@ -1,18 +1,18 @@
 package com.birdben;
 
+import org.apache.log4j.PropertyConfigurator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import junit.framework.TestResult;
 
 /**
  * Hello world!
  */
 public class App {
 
-    public static Logger logger = LoggerFactory.getLogger(App.class);
+    public static final Logger logger = LoggerFactory.getLogger(App.class);
 
     public static void main(String[] args) {
-        TestResult test = new TestResult();
+        PropertyConfigurator.configure("resources/log4j.properties");
         logger.info("App start");
         System.out.println("Hello World!");
         logger.info("App end");
